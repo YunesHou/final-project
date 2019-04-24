40439ec66db1d1c9abf8698d3efe193844698b81
Yu Hou <yuhou2@illinois.edu>
2019-04-23 22:56:21 -0500

    final 4

--- app/src/main/java/com/example/funnyapplication/result.java
+++ app/src/main/java/com/example/funnyapplication/result.java
@@ -41,20 +41,20 @@ public class result extends AppCompatActivity {
         Intent intent = getIntent();
 
         Button start = findViewById(R.id.start);
-        final TextView PlaceId = findViewById(R.id.PlaceId);
-        final TextView PlaceName = findViewById(R.id.PlaceName);
-        final TextView CountryId = findViewById(R.id.CountryId);
-        final TextView CityId = findViewById(R.id.CityId);
+        final TextView QuoteId = findViewById(R.id.QuoteId);
+        final TextView MinPrice = findViewById(R.id.MinPrice);
+        final TextView CarrierId = findViewById(R.id.CarrierId);
+        final TextView Name = findViewById(R.id.Name);
         final TextView CountryName = findViewById(R.id.CountryName);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                startAPICall("US", "USD", "en-US", "LAX-sky", "SFO-sky", "2019-01-01");
-                PlaceId.setVisibility(View.VISIBLE);
-                PlaceName.setVisibility(View.VISIBLE);
-                CountryId.setVisibility(View.VISIBLE);
-                CityId.setVisibility(View.VISIBLE);
+                startAPICall("US", "USD", "en-US", "SFO-sky", "JFK-sky", "2019-09-01", "2019-10-01");
+                QuoteId.setVisibility(View.VISIBLE);
+                MinPrice.setVisibility(View.VISIBLE);
+                CarrierId.setVisibility(View.VISIBLE);
+                Name.setVisibility(View.VISIBLE);
                 CountryName.setVisibility(View.VISIBLE);
             }
         });
@@ -65,10 +65,8 @@ public class result extends AppCompatActivity {
     }
 
 
-    void startAPICall(final String country, final String currency, final String locale, final String destinationplace, final String originplace, final String outboundpartialdate) {
-        String url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm";
-
-        final ArrayList<HashMap<String, String>> flightlist = new ArrayList<>();
+    void startAPICall(final String country, final String currency, final String locale, final String destinationplace, final String originplace, final String outboundpartialdate,final String inboundpartialdate) {
+        String url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-10-01?inboundpartialdate=2019-10-01";
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -96,53 +94,41 @@ public class result extends AppCompatActivity {
 
     void apiCallDone(final String response) {
         try {
-            TextView PlaceIdText = findViewById(R.id.PlaceId);
-            TextView PlaceNameText = findViewById(R.id.PlaceName);
-            TextView CountryIdText = findViewById(R.id.CountryId);
+            TextView QuoteIdText = findViewById(R.id.QuoteId);
+            TextView MinPriceText = findViewById(R.id.MinPrice);
+            TextView CarrierIdText = findViewById(R.id.CarrierId);
+            TextView NameText = findViewById(R.id.Name);
             TextView CountryNameText = findViewById(R.id.CountryName);
-            TextView CityIdText = findViewById(R.id.CityId);
 
-            final ArrayList flightlist = new ArrayList<>();
 
             JsonParser parser = new JsonParser();
             JsonObject jsonObj = parser.parse(response).getAsJsonObject();
 
-            //JSONObject jsonObj = new JSONObject();
-            JsonArray resultArray = jsonObj.getAsJsonArray("Places");
-            for (int i = 0; i < resultArray.size(); i++) {
-                JsonObject a = resultArray.get(i).getAsJsonObject();
-                String PlaceId = a.get("PlaceId").getAsString();
-                String PlaceName = a.get("PlaceName").getAsString();
-                String CountryId = a.get("CountryId").getAsString();
-                //String RegionId = a.get("RegionId").getAsString();
-                String CityId = a.get("CityId").getAsString();
-                String CountryName = a.get("CountryName").getAsString();
+            final ArrayList flightQuotes = new ArrayList<>();
+            JsonArray quotes = jsonObj.getAsJsonArray("Quotes");
+            for (int i = 0; i < quotes.size(); i++) {
+                JsonObject a = quotes.get(i).getAsJsonObject();
+                String QuoteId = a.get("QuoteId").getAsString();
+                String MinPrice = a.get("MinPrice").getAsString();
 
-                //HashMap<String, String> flight = new HashMap<>();
-
-                //flight.put("PlaceId", PlaceId);
-                //flight.put("PlaceName", PlaceName);
-                //flight.put("CountryId", CountryId);
-                //flight.put("RegionId", RegionId);
-                //flight.put("CityId", CityId);
-                //flight.put("CountryName", CountryName);
-
-                flightlist.add(PlaceId);
-                flightlist.add(PlaceName);
-                flightlist.add(CountryId);
-                //flightlist.add(RegionId);
-                flightlist.add(CityId);
-                flightlist.add(CountryName);
+                flightQuotes.add(QuoteId);
+                flightQuotes.add(MinPrice);
             }
-            for (int i = 0; i < flightlist.size(); i++) {
-                PlaceIdText.setText("Place ID: "+ flightlist.get(0));
-                PlaceNameText.setText("Place name: "+ flightlist.get(1));
-                CountryIdText.setText("Country ID: "+ flightlist.get(2));
-                CityIdText.setText("City ID: "+ flightlist.get(3));
-                CountryNameText.setText("Country Name: "+ flightlist.get(4));
+            QuoteIdText.setText("Quote ID: "+ flightQuotes.get(0));
+            MinPriceText.setText("MinPrice: "+ flightQuotes.get(1));
 
+            final ArrayList flightCarriers = new ArrayList<>();
+            JsonArray Carriers = jsonObj.getAsJsonArray("Carriers");
+            for (int i = 0; i < Carriers.size(); i++) {
+                JsonObject a = Carriers.get(i).getAsJsonObject();
+                String CarrierId = a.get("CarrierId").getAsString();
+                String Name = a.get("Name").getAsString();
+
+                flightCarriers.add(CarrierId);
+                flightCarriers.add(Name);
             }
-            //result.setText("Response is: "+ flightlist);
+            CarrierIdText.setText("Carrier ID: "+ flightCarriers.get(0));
+            NameText.setText("Name: "+ flightCarriers.get(1));
         } catch (final Exception e) {
             Log.e(TAG, "Couldn't get json from server.");
         }
